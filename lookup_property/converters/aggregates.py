9cead1d8d53e524d93b53255981f11990eff68d0
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2023-12-06 15:42:51 +0200

    Add postgres aggregates

--- lookup_property/converters/aggregates.py
+++ lookup_property/converters/aggregates.py
@@ -38,3 +38,48 @@ def _(
         slice=ast.Constant(value=arg_name),
         ctx=ast.Load(),
     )
+
+
+try:  # pragma: no cover
+    from django.contrib.postgres import aggregates as pg_aggregates
+
+    @expression_to_ast.register
+    def _(
+        expression: (
+            pg_aggregates.ArrayAgg
+            | pg_aggregates.BitAnd
+            | pg_aggregates.BitOr
+            | pg_aggregates.BitXor
+            | pg_aggregates.BoolAnd
+            | pg_aggregates.BoolOr
+            | pg_aggregates.JSONBAgg
+            | pg_aggregates.StringAgg
+            | pg_aggregates.StatAggregate
+            | pg_aggregates.Corr
+            | pg_aggregates.CovarPop
+            | pg_aggregates.RegrAvgX
+            | pg_aggregates.RegrAvgY
+            | pg_aggregates.RegrCount
+            | pg_aggregates.RegrIntercept
+            | pg_aggregates.RegrR2
+            | pg_aggregates.RegrSlope
+            | pg_aggregates.RegrSXX
+            | pg_aggregates.RegrSXY
+            | pg_aggregates.RegrSYY
+        ),
+        state: State,
+    ) -> ast.Subscript:
+        arg_name = state.extra_kwargs.add(lambda: expression)
+
+        return ast.Subscript(
+            value=ast_method(
+                "aggregate",
+                ["__class__", "objects"],
+                **{arg_name: ast_function(arg_name)},
+            ),
+            slice=ast.Constant(value=arg_name),
+            ctx=ast.Load(),
+        )
+
+except ModuleNotFoundError:
+    pass
